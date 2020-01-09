cf3de25819b710b99f462d37a9a51208b2751ede
jvictor11 <jvictor11@outlook.com>
2020-01-09 20:43:19 -0300

    Company Model

---
+++ server/models/BiddingModel.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+const BiddingSchema = new Schema({
+
+
+
+});
+
+module.exports = mongoose.model('Bidding', BiddingSchema);
---
+++ server/models/CompanyModel.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+const CompanySchema = new Schema({
+
+
+
+});
+
+module.exports = mongoose.model('Company', CompanySchema);
